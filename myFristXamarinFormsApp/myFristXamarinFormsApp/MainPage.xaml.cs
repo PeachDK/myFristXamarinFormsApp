8cc499e67e585fcc754de571f7bfe808c0d8d403
Pelle Jacob Hansen <pellejacobhansen@outlook.dk>
2018-05-18 08:33:03 +0200

    Add project files.

---
+++ myFristXamarinFormsApp/myFristXamarinFormsApp/MainPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace myFristXamarinFormsApp
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+
+        public void OnButtonSelected(object sender, EventArgs args)
+        {
+            Button button = (Button)sender;
+            if (resultText.Text == "0")
+            {
+                resultText.Text = "";
+            }
+
+            resultText.Text += button.Text;
+        }
+
+        public void OnClearSelected(object sender, EventArgs args)
+        {
+            resultText.Text = "0";
+        }
+       
+        public void OnCalculate(object sender, EventArgs args)
+        {
+            
+                string expression = resultText.Text;
+
+                if (expression.Contains("X"))
+                {
+                    expression = expression.Replace("X", "*");
+                }
+                DataTable dt = new DataTable();
+                var v = dt.Compute(expression, "");
+
+                resultText.Text = v.ToString();
+            
+
+        }
+
+    }
+
+}
